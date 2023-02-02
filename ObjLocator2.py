7a4e2f854d958f2d491eedf446207fbf91f3dc8a
DavidLCPyML <email2david2015@gmail.com>
2023-02-01 21:13:30 -0600

    can recognize green caps now :D

--- ObjLocator2.py
+++ ObjLocator2.py
@@ -2,7 +2,8 @@ import cv2 as cv
 import numpy as np
 import imutils
 
-videoCapture = cv.VideoCapture(1, cv.CAP_DSHOW)
+# use "1" if no built in webcam
+videoCapture = cv.VideoCapture(0, cv.CAP_DSHOW)
 
 greenLower = (29, 86, 6)
 greenUpper = (64, 255, 255)
@@ -12,10 +13,10 @@ while True:
     if not ret:
         break
 
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
     gray = cv.GaussianBlur(gray, (11, 11), 0)
 
-    mask = cv.inRange(frame, greenLower, greenUpper)
+    mask = cv.inRange(gray, greenLower, greenUpper)
     mask = cv.erode(mask, None, iterations=2)
     mask = cv.dilate(mask, None, iterations=2)
 
@@ -31,10 +32,16 @@ while True:
         center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
 
         if r > 10:
-            cv.circle(frame, int(x), int(y), int(r), (0, 255, 255), 2)
+            cv.circle(frame, (int(x), int(y)), int(r), (0, 255, 255), 2)
             cv.circle(frame, center, 2, (0, 255, 255), 2)
-            
 
+            print("x: " + str(int(x)) + ", y: " + str(int(y)))
+
+
+    cv.imshow("circles", frame)
+    cv.imshow("grayscale", gray)
+    cv.imshow("mask", mask)
+    # cv.imshow("circles", frame)
 
     if cv.waitKey(1) == ord('q'):
         break
