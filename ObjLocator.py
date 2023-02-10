6f8074c1df86a0607c28ae584060d610f5459f21
DavidLCPyML <email2david2015@gmail.com>
2023-02-09 20:00:55 -0600

    general purpose small-circle identifier, not so great for bigger circles though"

--- ObjLocator.py
+++ ObjLocator.py
@@ -1,7 +1,7 @@
 import cv2 as cv
 import numpy as np
 
-videoCapture = cv.VideoCapture(0)
+videoCapture = cv.VideoCapture(1, cv.CAP_DSHOW)
 prevCircle = None
 dist = lambda x1, y1, x2, y2: (x1 - x2) ** 2 + (y1 - y2) ** 2
 
@@ -15,15 +15,24 @@ while True:
         break
 
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    gray = cv.GaussianBlur(gray, (17,17), 0)
+    # figuring out what types of blurs are needed. This one seems to work the best?
+    gray = cv.GaussianBlur(gray, (9,9), 0)
+    # gray = cv.medianBlur(gray, 13)
+    # gray = cv.bilateralFilter(gray,d=10,sigmaColor=200,sigmaSpace=200)
+
 
     # mask = cv.inRange(frame, greenLower, greenUpper)
     # mask = cv.erode(mask, None, iterations=2)
     # mask = cv.dilate(mask, None, iterations=2)
     # result = cv.bitwise_and(gray, gray, mask= mask)
+    
+    # docstring of HoughCircles: HoughCircles(image, method, dp, minDist[, circles[, param1[, param2[, minRadius[, maxRadius]]]]]) -> circles
+    circles = cv.HoughCircles(gray, cv.HOUGH_GRADIENT, dp=1, minDist=10, 
+                              param1 = 60,
+                              param2 = 75,
+                              minRadius = 1, maxRadius = 500)
 
-    circles = cv.HoughCircles(gray, cv.HOUGH_GRADIENT, 1, 100, 
-                              param1 = 100, param2 = 30, minRadius = 50, maxRadius = 500)
+    # check out morphologyEx (can bypass 2-3 steps)
 
     if circles is not None:
         circles = np.uint16(np.around(circles))
@@ -34,8 +43,8 @@ while True:
             if prevCircle is not None:
                 if dist(chosen[0], chosen[1], prevCircle[0], prevCircle[1]) <= dist(i[0], i[1], prevCircle[0], prevCircle[1]):
                     chosen = i
-        cv.circle(frame, (chosen[0], chosen[1]), 1, (0, 100, 100), 3)
-        cv.circle(frame, (chosen[0], chosen[1]), chosen[2], (255, 0 , 255), 3)
+            cv.circle(frame, (i[0], i[1]), 1, (0, 100, 100), 3)
+            cv.circle(frame, (i[0], i[1]), i[2], (255, 0 , 255), 3)
         
         # print("x: " + str(chosen[0]) + ", y: " + str(chosen[1]))
         prevCircle = chosen
